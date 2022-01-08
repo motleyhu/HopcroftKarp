1cfeabaa29653daece632d8e49a173bb41d83614
Tamás Szigeti <tamas@szigeti.one>
2022-01-08 13:03:37 +0100

    Less input, less validation

--- src/ValueObject/BipartiteGraph.php
+++ src/ValueObject/BipartiteGraph.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Motley\HopcroftKarp\ValueObject;
 
-use Motley\HopcroftKarp\InvalidArgumentException;
-
 /**
  * The bipartite graph that we are trying to find a maximum cardinality matching of
  */
@@ -19,36 +17,38 @@ class BipartiteGraph
     /**
      * @var Vertex[]
      */
-    private array $set1;
+    private array $set1 = [];
 
     /**
      * @var Vertex[]
      */
-    private array $set2;
+    private array $set2 = [];
 
     /**
-     * TODO: Maybe extract vertices instead of expecting them
-     *
-     * @param Vertex[] $set1
-     * @param Vertex[] $set2
-     * @param Edge[]   $edges
+     * @param Edge[] $edges
      */
-    public function __construct(array $set1, array $set2, array $edges)
+    public function __construct(array $edges)
     {
         foreach ($edges as $edge) {
-            if (!\in_array($edge->getVertex1(), $set1)) {
-                throw new InvalidArgumentException('Starting vertex of edge must be in set1');
+            $vertex1 = $edge->getVertex1();
+            $vertex2 = $edge->getVertex2();
+
+            if (!\in_array($vertex1, $this->set1)) {
+                $this->set1[] = $vertex1;
             }
-            if (!\in_array($edge->getVertex2(), $set2)) {
-                throw new InvalidArgumentException('Ending vertex of edge must be in set2');
+            if (!\in_array($vertex2, $this->set2)) {
+                $this->set2[] = $vertex2;
             }
-        }
-        if (array_intersect($set1, $set2)) {
-            throw new InvalidArgumentException('Vertex sets shall not intersect');
         }
 
-        $this->set1 = $set1;
-        $this->set2 = $set2;
         $this->edges = $edges;
+    }
+
+    /**
+     * @return Edge[]
+     */
+    public function getEdges(): array
+    {
+        return $this->edges;
     }
 }
