3c8863a976a711f4c8ebd05132b9a6d975f42e0a
Benedek Dul <benedekdul@gmail.com>
2023-04-27 13:56:08 +0200

    Testcase for freezing edges

--- tests/BipartiteGraphTest.php
+++ tests/BipartiteGraphTest.php
@@ -122,4 +122,28 @@ class BipartiteGraphTest extends TestCase
             ],
         ];
     }
+
+    public function testFreezingEdges(): void
+    {
+        $previousMatching = [
+            1 => 3,
+            2 => 1,
+            3 => 2,
+            4 => 4,
+        ];
+
+        $graph = new BipartiteGraph([
+            1 => [1, 2, 4],
+            2 => [1, 2, 3, 4],
+            3 => [1, 3, 4],
+            4 => [1, 2, 3, 4],
+        ]);
+
+        $this->assertSame([
+            1 => 2,
+            2 => 1,
+            3 => 3,
+            4 => 4,
+        ], $graph->hopcroftKarp($previousMatching));
+    }
 }
